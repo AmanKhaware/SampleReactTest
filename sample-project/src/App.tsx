88fc996f3a62eed7f88a6f221ea49b89d9384f09
ESOLUTIONS\Aman.Khaware <Aman.Khaware@trivium-esolutions.com>
2023-05-25 17:00:52 +0530

    bootstrap added

--- sample-project/src/App.tsx
+++ sample-project/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import ListGroup from "./assets/Component/ListGroup";
 import Message from "./assets/Component/Message";
 
 function App() {
   return (
     <div>
       <Message />
+      <ListGroup/>
     </div>
   );
 }
---
+++ sample-project/src/assets/Component/ListGroup.tsx
@@ -0,0 +1,13 @@
+function ListGroup(){
+    return(
+        <ul className="list-group">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A Second Item</li>
+            <li className="list-group-item">A third Item</li>
+            <li className="list-group-item">A fourth Item</li>
+            <li className="list-group-item">And a fifth item</li>
+        </ul>
+    )
+}
+
+export default ListGroup;
